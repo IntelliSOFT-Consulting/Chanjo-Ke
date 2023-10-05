98ba7631ccce2b3c022f9df460f31f8d6ce37fdd
davidnjau <davidnjau21@gmail.com>
2023-10-05 12:05:50 +0300

    Load immunisation json

--- app/src/main/java/com/dave/zanzibar/vaccine/AdministerVaccine.kt
+++ app/src/main/java/com/dave/zanzibar/vaccine/AdministerVaccine.kt
@@ -2,7 +2,14 @@ package com.dave.zanzibar.vaccine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import ca.uhn.fhir.context.FhirContext
 import com.dave.zanzibar.R
+import com.dave.zanzibar.add_patient.AddPatientFragment
 import com.dave.zanzibar.databinding.ActivityAdministerVaccineBinding
 import com.dave.zanzibar.databinding.ActivityMainBinding
 import com.dave.zanzibar.fhir.data.FormatterClass
@@ -16,10 +23,19 @@ class AdministerVaccine : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAdministerVaccineBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val toolbar =
+            findViewById<Toolbar>(R.id.toolbar) // Assuming you have a Toolbar with id 'toolbar' in your layout
+        setSupportActionBar(toolbar)
 
         patientId = formatterClass.getSharedPref("patientId",this)
+
+        val bundle = Bundle()
+        bundle.putString(AddPatientFragment.QUESTIONNAIRE_FILE_PATH_KEY, "new-patient-registration-paginated.json")
+        findNavController(R.id.nav_administer_vaccine).navigate(R.id.addPatientFragment, bundle)
 
         println(patientId)
 
     }
+
+
 }
