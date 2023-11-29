f9e4b736339429527c0e22495a1555ded8b36a84
itskiprotich <keeprawteachjapheth@gmail.com>
2023-11-29 15:11:41 +0300

    UPD: Identification Details

--- app/src/main/java/com/intellisoft/chanjoke/patient_list/PatientListViewModel.kt
+++ app/src/main/java/com/intellisoft/chanjoke/patient_list/PatientListViewModel.kt
@@ -37,74 +37,76 @@ import org.hl7.fhir.r4.model.Patient
  * data for UI.
  */
 class PatientListViewModel(application: Application, private val fhirEngine: FhirEngine) :
-  AndroidViewModel(application) {
+    AndroidViewModel(application) {
 
-  val liveSearchedPatients = MutableLiveData<List<PatientItem>>()
-  val patientCount = MutableLiveData<Long>()
+    val liveSearchedPatients = MutableLiveData<List<PatientItem>>()
+    val patientCount = MutableLiveData<Long>()
 
-  init {
-    updatePatientListAndPatientCount({ getSearchResults() }, { count() })
-  }
-
-  fun searchPatientsByName(nameQuery: String) {
-    updatePatientListAndPatientCount({ getSearchResults(nameQuery) }, { count(nameQuery) })
-  }
-
-  /**
-   * [updatePatientListAndPatientCount] calls the search and count lambda and updates the live data
-   * values accordingly. It is initially called when this [ViewModel] is created. Later its called
-   * by the client every time search query changes or data-sync is completed.
-   */
-  private fun updatePatientListAndPatientCount(
-    search: suspend () -> List<PatientItem>,
-    count: suspend () -> Long,
-  ) {
-    viewModelScope.launch {
-      liveSearchedPatients.value = search()
-      patientCount.value = count()
+    init {
+        updatePatientListAndPatientCount({ getSearchResults() }, { count() })
     }
-  }
 
-  /**
-   * Returns count of all the [Patient] who match the filter criteria unlike [getSearchResults]
-   * which only returns a fixed range.
-   */
-  private suspend fun count(nameQuery: String = ""): Long {
-    return fhirEngine.count<Patient> {
-      if (nameQuery.isNotEmpty()) {
-        filter(
-          Patient.NAME,
-          {
-            modifier = StringFilterModifier.CONTAINS
-            value = nameQuery
-          },
-        )
-      }
+    fun searchPatientsByName(nameQuery: String) {
+        updatePatientListAndPatientCount({ getSearchResults(nameQuery) }, { count(nameQuery) })
     }
-  }
 
-  private suspend fun getSearchResults(nameQuery: String = ""): List<PatientItem> {
-    val patients: MutableList<PatientItem> = mutableListOf()
-    fhirEngine
-      .search<Patient> {
-
-        if (nameQuery.isNotEmpty()) {
-          filter(Patient.NAME, {
-              modifier = StringFilterModifier.CONTAINS
-              value = nameQuery
-            },
-          )
+    /**
+     * [updatePatientListAndPatientCount] calls the search and count lambda and updates the live data
+     * values accordingly. It is initially called when this [ViewModel] is created. Later its called
+     * by the client every time search query changes or data-sync is completed.
+     */
+    private fun updatePatientListAndPatientCount(
+        search: suspend () -> List<PatientItem>,
+        count: suspend () -> Long,
+    ) {
+        viewModelScope.launch {
+            liveSearchedPatients.value = search()
+            patientCount.value = count()
         }
+    }
 
-        sort(Patient.GIVEN, Order.ASCENDING)
-        count = 100
-        from = 0
-      }
-      .mapIndexed { index, fhirPatient -> fhirPatient.toPatientItem(index + 1) }
-      .let { patients.addAll(it) }
+    /**
+     * Returns count of all the [Patient] who match the filter criteria unlike [getSearchResults]
+     * which only returns a fixed range.
+     */
+    private suspend fun count(nameQuery: String = ""): Long {
+        return fhirEngine.count<Patient> {
+            if (nameQuery.isNotEmpty()) {
+                filter(
+                    Patient.NAME,
+                    {
+                        modifier = StringFilterModifier.CONTAINS
+                        value = nameQuery
+                    },
+                )
+            }
+        }
+    }
 
-    return patients
-  }
+    private suspend fun getSearchResults(nameQuery: String = ""): List<PatientItem> {
+        val patients: MutableList<PatientItem> = mutableListOf()
+        fhirEngine
+            .search<Patient> {
+
+                if (nameQuery.isNotEmpty()) {
+                    filter(
+                        Patient.NAME,
+                        {
+                            modifier = StringFilterModifier.CONTAINS
+                            value = nameQuery
+                        },
+                    )
+                }
+
+                sort(Patient.GIVEN, Order.ASCENDING)
+                count = 100
+                from = 0
+            }
+            .mapIndexed { index, fhirPatient -> fhirPatient.toPatientItem(index + 1) }
+            .let { patients.addAll(it) }
+
+        return patients
+    }
 
 
 //  private suspend fun getRiskAssessments(): Map<String, RiskAssessment?> {
@@ -119,88 +121,92 @@ class PatientListViewModel(application: Application, private val fhirEngine: Fhi
 //      }
 //  }
 
-  /** The Patient's details for display purposes. */
-  data class PatientItem(
-    val id: String,
-    val resourceId: String,
-    val name: String,
-    val gender: String,
-    val dob: LocalDate? = null,
-    val phone: String,
-    val city: String,
-    val country: String,
-    val isActive: Boolean,
-    val html: String,
-    var risk: String? = "",
+    /** The Patient's details for display purposes. */
+    data class PatientItem(
+        val id: String,
+        val resourceId: String,
+        val name: String,
+        val gender: String,
+        val dob: LocalDate? = null,
+        val identification: String,
+        val phone: String,
+        val city: String,
+        val country: String,
+        val isActive: Boolean,
+        val html: String,
+        var risk: String? = "",
 //    var riskItem: RiskAssessmentItem? = null,
-  ) {
-    override fun toString(): String = name
-  }
-
-  /** The Observation's details for display purposes. */
-
-
-  /** The Observation's details for display purposes. */
-  data class ObservationItem(
-    val id: String,
-    val code: String,
-    val effective: String,
-    val value: String,
-    val dateTime: String? = null,
-  ) {
-    override fun toString(): String = code
-  }
-
-  data class ConditionItem(
-    val id: String,
-    val code: String,
-    val effective: String,
-    val value: String,
-  ) {
-    override fun toString(): String = code
-  }
-
-  class PatientListViewModelFactory(
-    private val application: Application,
-    private val fhirEngine: FhirEngine,
-  ) : ViewModelProvider.Factory {
-    @Suppress("UNCHECKED_CAST")
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-      if (modelClass.isAssignableFrom(PatientListViewModel::class.java)) {
-        return PatientListViewModel(application, fhirEngine) as T
-      }
-      throw IllegalArgumentException("Unknown ViewModel class")
+    ) {
+        override fun toString(): String = name
     }
-  }
+
+    /** The Observation's details for display purposes. */
+
+
+    /** The Observation's details for display purposes. */
+    data class ObservationItem(
+        val id: String,
+        val code: String,
+        val effective: String,
+        val value: String,
+        val dateTime: String? = null,
+    ) {
+        override fun toString(): String = code
+    }
+
+    data class ConditionItem(
+        val id: String,
+        val code: String,
+        val effective: String,
+        val value: String,
+    ) {
+        override fun toString(): String = code
+    }
+
+    class PatientListViewModelFactory(
+        private val application: Application,
+        private val fhirEngine: FhirEngine,
+    ) : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(PatientListViewModel::class.java)) {
+                return PatientListViewModel(application, fhirEngine) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
 }
 
 internal fun Patient.toPatientItem(position: Int): PatientListViewModel.PatientItem {
-  // Show nothing if no values available for gender and date of birth.
-  val patientId = if (hasIdElement()) idElement.idPart else ""
-  val name = if (hasName()) name[0].nameAsSingleString else ""
-  val gender = if (hasGenderElement()) genderElement.valueAsString else ""
-  val dob =
-    if (hasBirthDateElement()) {
-      LocalDate.parse(birthDateElement.valueAsString, DateTimeFormatter.ISO_DATE)
-    } else {
-      null
-    }
-  val phone = if (hasTelecom()) telecom[0].value else ""
-  val city = if (hasAddress()) address[0].city else ""
-  val country = if (hasAddress()) address[0].country else ""
-  val isActive = active
-  val html: String = if (hasText()) text.div.valueAsString else ""
+    // Show nothing if no values available for gender and date of birth.
+    val patientId = if (hasIdElement()) idElement.idPart else ""
+    val name = if (hasName()) name[0].nameAsSingleString else ""
+    val gender = if (hasGenderElement()) genderElement.valueAsString else ""
+    val dob =
+        if (hasBirthDateElement()) {
+            LocalDate.parse(birthDateElement.valueAsString, DateTimeFormatter.ISO_DATE)
+        } else {
+            null
+        }
+    val phone = if (hasTelecom()) telecom[0].value else ""
+    val city = if (hasAddress()) address[0].city else ""
+    val country = if (hasAddress()) address[0].country else ""
+    val isActive = active
+    val html: String = if (hasText()) text.div.valueAsString else ""
 
-  return PatientListViewModel.PatientItem(
-    id = position.toString(),
-    resourceId = patientId,
-    name = name,
-    gender = gender ?: "",
-    dob = dob,
-    phone = phone ?: "",
-    city = city ?: "",
-    country = country ?: "",
-    isActive = isActive,
-    html = html,
-  )
+    val identification: String = if (hasIdentifier()) identifier[0].value else "N/A"
+
+    return PatientListViewModel.PatientItem(
+        id = position.toString(),
+        resourceId = patientId,
+        name = name,
+        gender = gender ?: "",
+        dob = dob,
+        identification = identification,
+        phone = phone ?: "",
+        city = city ?: "",
+        country = country ?: "",
+        isActive = isActive,
+        html = html,
+    )
 }
