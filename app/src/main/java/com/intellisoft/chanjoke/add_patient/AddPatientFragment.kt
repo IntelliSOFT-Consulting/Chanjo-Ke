1d7b5609c9c2315a800c91abe845f7bce95bb4fd
itskiprotich <keeprawteachjapheth@gmail.com>
2024-01-03 14:23:59 +0300

    age

--- app/src/main/java/com/intellisoft/chanjoke/add_patient/AddPatientFragment.kt
+++ app/src/main/java/com/intellisoft/chanjoke/add_patient/AddPatientFragment.kt
@@ -110,8 +110,8 @@ class AddPatientFragment : Fragment(R.layout.add_patient_fragment) {
     private fun updateArguments() {
         requireArguments().putString(
             QUESTIONNAIRE_FILE_PATH_KEY,
-            "new-patient-registration-paginated.json"
-//            "age-calculator.json"
+//            "new-patient-registration-paginated.json"
+            "registration.json"
         )
     }
 
