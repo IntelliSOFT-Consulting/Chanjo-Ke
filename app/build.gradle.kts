1d61b3e3dffc76b06a0a8cc8852778b1cfb5e1b6
davidnjau <davidnjau21@gmail.com>
2024-02-19 18:05:26 +0300

    Check if name is available

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["appAuthRedirectScheme"] = applicationId!!
         buildFeatures.buildConfig = true
-        versionCode = 6
+        versionCode = 7
         versionName = "1.0"
     }
     buildTypes {
