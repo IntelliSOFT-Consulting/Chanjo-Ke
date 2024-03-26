e2659a87940a636ec59c7131ecc7818ac0913a3a
davidnjau <davidnjau21@gmail.com>
2024-03-26 10:00:23 +0300

    Updated ver number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["appAuthRedirectScheme"] = applicationId!!
         buildFeatures.buildConfig = true
-        versionCode = 10
+        versionCode = 12
         versionName = "1.0"
     }
     buildTypes {
