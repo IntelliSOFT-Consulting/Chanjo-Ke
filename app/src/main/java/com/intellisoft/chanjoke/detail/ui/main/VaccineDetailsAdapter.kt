efaa0850f09145e52cd742147b5dc2614cb169d7
David <davidnjau21@gmail.com>
2024-06-10 17:55:10 +0300

    Chnaged status for bcg to updcoming

--- app/src/main/java/com/intellisoft/chanjoke/detail/ui/main/VaccineDetailsAdapter.kt
+++ app/src/main/java/com/intellisoft/chanjoke/detail/ui/main/VaccineDetailsAdapter.kt
@@ -37,6 +37,7 @@ class VaccineDetailsAdapter(
     override fun onBindViewHolder(holder: VaccineDetailsViewHolder, position: Int) {
         val currentItem = vaccineDetailsList[position]
         val formatterClass = FormatterClass()
+        val immunizationHandler = ImmunizationHandler()
 //        holder.checkBox.isChecked = currentItem.isVaccinated
         val vaccineName = currentItem.vaccineName
         val isVaccinated = currentItem.isVaccinated
@@ -63,6 +64,36 @@ class VaccineDetailsAdapter(
         }else if (status == StatusColors.RED.name){
             vaccineStatus = "Missed"
             holder.tvScheduleStatus.setTextColor(context.resources.getColor(R.color.red))
+
+            //For BCG and bOpv
+            if (vaccineName == "BCG" || vaccineName == "bOPV"){
+                //Check the date and change status
+
+                val basicVaccine = immunizationHandler.getVaccineDetailsByBasicVaccineName(vaccineName)
+                if (basicVaccine != null){
+                    /**
+                     * BCG can be given till 255
+                     * bOPV will be till 2 weeks
+                     */
+                    if (daysTo != null){
+                        val daysToInt = daysTo.toInt()
+                        if (daysToInt < 15){
+                            vaccineStatus = "Upcoming"
+                            holder.tvScheduleStatus.setTextColor(context.resources.getColor(R.color.black))
+                        }
+                    }
+
+                }
+
+                Log.e("------>","<-------")
+                println("vaccineName $vaccineName")
+                println("vaccineStatus $vaccineStatus")
+                println("daysTo $daysTo")
+                Log.e("------>","<-------")
+            }
+
+
+
         }else if (status == StatusColors.NOT_DONE.name){
             vaccineStatus = "Not Administered"
             holder.tvScheduleStatus.setTextColor(context.resources.getColor(R.color.colorAccent))
